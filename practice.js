293743275549ab327ed62ed61bba2f4d679692d7
kakaoMao <mentos531@yandex.ru>
2022-05-16 00:04:26 +0300

    Mocha practice started

--- practice.js
+++ practice.js
@@ -4,3 +4,21 @@ let sum = (a, b) => a + b
 
 console.log(` Стрелочная функция ${sum}`)
 
+
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+  }
+  
+  ask(
+    "Вы согласны?",
+    function() { alert("Вы согласились."); },
+    function() { alert("Вы отменили выполнение."); }
+  );
+
+
+
+  let ask = (question, yes, no) => {
+      if (confirm(question)) return yes()
+      else return no()
+  }
