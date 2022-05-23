aa908da5ee931effef8aa625feee021d7add92ee
kakaoMao <mentos531@yandex.ru>
2022-05-23 21:43:35 +0300

    mocha  продолжение

--- mochaTests.js
+++ mochaTests.js
@@ -1,7 +1,20 @@
 describe("pow", function() {
 
-    it("возводит в степень n", function() {
-      assert.equal(pow(2, 3), 8);
-    });
-  
-  });
+    describe("возводит x в степень 3", function() {
+    
+        before(() => alert("Тестирование началось – перед тестами"));
+        beforeEach(() => alert(`Перед тестом – начинаем выполнять тест: число Х в степени 3`));
+
+        function makeTest(x) {
+            let expected = x * x * x;
+            it(`${x} в степени 3 будет ${expected}`, function() {
+                assert.equal(pow(x, 3), expected);
+            });
+        }
+        
+        for (let x = 1; x <= 5; x++) {
+            makeTest(x);
+        }
+
+    }); 
+});
