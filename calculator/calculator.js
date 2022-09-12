b0be265cb338728347555ba0563db5979a7f0e12
kakaoMao <mentos531@yandex.ru>
2022-09-12 22:28:01 +0300

    пока что поуза на проекте с калькулятором. пока что вообще не понимаю как обрабатывать более 2 чисел

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -3,13 +3,15 @@
 // console.log(first);
 // console.log((first)[0].innerText);
 
+let numberFinished = Symbol('numberFinished')
 
 let calculator = {
     
     actualNumber : '',
     actualCalc : '',
     result : 0,
-    actualBtn : '+',
+    actualBtn : '',
+    //[numberFinished]: false,
 
     numInput (num) {
         this.actualNumber += num;
@@ -19,10 +21,13 @@ let calculator = {
     },
 
     btnInput (btn) {
+        //this[numberFinished] = true
         this.actualBtn = btn;
         this.actualCalc += btn;
+        this.result = this.actualNumber;
         this.actualNumber = '';      
         this.drawing('item_enteredNumbers', this.actualCalc);
+        this.logging();
     },
 
     calculate () {
@@ -30,21 +35,33 @@ let calculator = {
     },
 
     calculating () {
-        if (this.actualBtn == '+') { this.result = this.result + Number(this.actualNumber); }
+        console.log('hey, start calculating')
+        if (this.actualBtn == '+' && this[numberFinished] == true) {
+            console.log('hey its + symbol start summing')
+            this.result = +this.result + +this.actualNumber
+        }
+        this.logging()
+        // if (this.actualBtn == '+') { this.result = this.result + Number(this.actualNumber); }
 
-            else if (this.actualBtn == '-') { this.result = this.result - Number(this.actualNumber); }
+        //     else if (this.actualBtn == '-') { this.result = this.result - Number(this.actualNumber); }
 
-            else if (this.actualBtn  == '*') { this.result = this.result * Number(this.prevNumber); }
+        //     else if (this.actualBtn  == '*') { this.result = this.result * Number(this.prevNumber); }
 
-            else if (this.actualBtn  == '/') { this.result = this.result / Number(this.prevNumber); }
+        //     else if (this.actualBtn  == '/') { this.result = this.result / Number(this.prevNumber); }
 
-            else if (this.actualBtn  == '**') { this.result = this.result ** Number(this.prevNumber); }
+        //     else if (this.actualBtn  == '**') { this.result = this.result ** Number(this.prevNumber); }
     },
 
     drawing (className, item) {
         document.getElementsByClassName(className)[0].innerText = item;
+    },
+
+    logging () {
+        console.log(`----- Start logging -----`)
+        console.log(`Actual number: ${this.actualNumber}`)
+        console.log(`Actual button: ${this.actualBtn}`)
+        console.log(`Result: ${this.result}`)
+        console.log(`----- End logging -----`)
     }
 
 };
-
-
